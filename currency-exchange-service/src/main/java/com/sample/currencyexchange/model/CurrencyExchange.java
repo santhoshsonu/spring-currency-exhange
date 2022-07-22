4b4c595136df36748b740461ca6124528d22ba9d
santhoshsonu <santhosheshwaramma@gmail.com>
2022-07-22 19:52:40 +0530

    Add app instance port details

--- currency-exchange-service/src/main/java/com/sample/currencyexchange/model/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/sample/currencyexchange/model/CurrencyExchange.java
@@ -1,6 +1,5 @@
 package com.sample.currencyexchange.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
@@ -9,12 +8,18 @@ import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Accessors(chain = true)
 public class CurrencyExchange {
-
   private Long id;
   private String from;
   private String to;
   private BigDecimal conversionMultiple;
+  private String environment;
+
+  public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
+    this.id = id;
+    this.from = from;
+    this.to = to;
+    this.conversionMultiple = conversionMultiple;
+  }
 }
--- currency-exchange-service/src/main/java/com/sample/currencyexchange/service/impl/CurrencyExchangeServiceImpl.java
+++ currency-exchange-service/src/main/java/com/sample/currencyexchange/service/impl/CurrencyExchangeServiceImpl.java
@@ -2,6 +2,8 @@ package com.sample.currencyexchange.service.impl;
 
 import com.sample.currencyexchange.model.CurrencyExchange;
 import com.sample.currencyexchange.service.CurrencyExchangeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -9,12 +11,13 @@ import java.math.BigDecimal;
 @Service
 public class CurrencyExchangeServiceImpl implements CurrencyExchangeService {
 
+  private static final String PORT_PROPERTY_KEY = "local.server.port";
+  @Autowired Environment environment;
+
   @Override
   public CurrencyExchange getExchangeRate(String from, String to) {
-    return new CurrencyExchange()
-        .setId(1000L)
-        .setFrom(from)
-        .setTo(to)
-        .setConversionMultiple(BigDecimal.valueOf(80.50));
+    final String property = environment.getProperty(PORT_PROPERTY_KEY);
+    return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(80.50))
+        .setEnvironment(property);
   }
 }
