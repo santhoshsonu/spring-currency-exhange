41215ee9bef7c7e458a7f47fc21b35c2696367df
santhoshsonu <santhosheshwaramma@gmail.com>
2022-07-23 21:17:04 +0530

    Configure Feign to load balance using Eureka Client

--- currency-conversion-service/src/main/java/com/sample/currencyconversion/remote/CurrencyExchangeFeignClient.java
+++ currency-conversion-service/src/main/java/com/sample/currencyconversion/remote/CurrencyExchangeFeignClient.java
@@ -5,8 +5,14 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "${remote.currency-exchange.base-url}")
+/**
+ * Configure Feign client to call currency-exchange service
+ * to fetch the exchange rates.
+ * Automatically picks up instance url from Eureka client using the service name
+ * */
+//@FeignClient(name = "currency-exchange-service", url = "${remote.currency-exchange.base-url}")
+@FeignClient(name = "currency-exchange-service")
 public interface CurrencyExchangeFeignClient {
-  @GetMapping(value = "/from/{from}/to/{to}")
+  @GetMapping(value = "/api/currency-exchange/from/{from}/to/{to}")
   CurrencyConversion getCurrencyExchangeRate(@PathVariable String from, @PathVariable String to);
 }
