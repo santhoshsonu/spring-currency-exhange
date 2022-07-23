ee80f8096d32500df21b12c0af2a3769329acfd5
santhoshsonu <santhosheshwaramma@gmail.com>
2022-07-23 12:08:19 +0530

    Fetch currency exchange details using RestTemplate

--- currency-conversion-service/src/main/java/com/sample/currencyconversion/service/Impl/CurrencyConversionServiceImpl.java
+++ currency-conversion-service/src/main/java/com/sample/currencyconversion/service/Impl/CurrencyConversionServiceImpl.java
@@ -4,9 +4,14 @@ import com.sample.currencyconversion.model.CurrencyConversion;
 import com.sample.currencyconversion.service.CurrencyConversionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.Map;
+
+import static java.util.Objects.nonNull;
 
 @Service
 public class CurrencyConversionServiceImpl implements CurrencyConversionService {
@@ -17,18 +22,23 @@ public class CurrencyConversionServiceImpl implements CurrencyConversionService 
 
   @Override
   public CurrencyConversion getConversion(String from, String to, BigDecimal quantity) {
-    final String port = environment.getProperty(PORT_PROPERTY_FIELD);
+    final Map<String, String> uriVariables =
+        Map.ofEntries(Map.entry("from", from), Map.entry("to", to));
 
-    final BigDecimal conversionMultiple = BigDecimal.valueOf(1.0);
-    final BigDecimal totalCalculatedAmount = quantity.multiply(conversionMultiple);
-
-    return new CurrencyConversion()
-        .setId(1L)
-        .setFrom(from)
-        .setTo(to)
-        .setQuantity(quantity)
-        .setConversionMultiple(conversionMultiple)
-        .setTotalCalculatedAmount(totalCalculatedAmount)
-        .setEnvironment(port);
+    ResponseEntity<CurrencyConversion> entity =
+        new RestTemplate()
+            .getForEntity(
+                "http://localhost:8000/api/currency-exchange/from/{from}/to/{to}",
+                CurrencyConversion.class,
+                uriVariables);
+    if (entity.getStatusCode().is2xxSuccessful()) {
+      CurrencyConversion currencyConversion = entity.getBody();
+      if (nonNull(currencyConversion)) {
+        return currencyConversion.setTotalCalculatedAmount(
+            quantity.multiply(currencyConversion.getConversionMultiple()));
+      }
+    }
+    throw new RuntimeException(
+        String.format("Unable to calculate conversion from %s to %s", from, to));
   }
 }
