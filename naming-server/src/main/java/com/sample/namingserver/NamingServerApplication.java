5f9761a7f9a7f5428633a06a13cc86735f21b9e8
santhoshsonu <santhosheshwaramma@gmail.com>
2022-07-23 13:04:37 +0530

    Eureka Naming Server init commit

---
+++ naming-server/src/main/java/com/sample/namingserver/NamingServerApplication.java
@@ -0,0 +1,12 @@
+package com.sample.namingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NamingServerApplication.class, args);
+  }
+}
